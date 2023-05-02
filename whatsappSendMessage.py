58d0b19ba41ffcc0d76b541bd0c7a7194abb4c6d
logesh-1023 <logeshs91@gmail.com>
2023-05-02 11:06:46 +0530

    Chat

--- whatsappSendMessage.py
+++ whatsappSendMessage.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-ACCOUNT_SID=""
-AUTH_TOKEN=""
+ACCOUNT_SID="ACacfa20d6e409fbefc9cd4c8a61f465af"
+AUTH_TOKEN="9c94db95a9b144722b688ab9bb14d792"
 FROM="whatsapp:+14155238886"
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
